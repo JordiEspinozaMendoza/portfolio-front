09e384230662be3432de5635b81bd5bb556a12d4
Jordi <jordi8101@gmail.com>
2022-11-21 01:25:50 -0800

    Updated components

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -5,27 +5,66 @@ import { GlobalContext } from "pages/_app";
 import styled from "styled-components";
 import styles from "./styles.module.sass";
 import Icon from "components/icon";
+import { useRouter } from "next/router";
+import { socialLinks } from "utils/seo/settings";
 const Container = styled.div`
   background-color: ${(props) => props.theme.navbar.background};
-  p {
+  border: ${(props) => props.theme.navbar.border};
+  p,
+  select,
+  option,
+  a, svg {
     color: ${(props) => props.theme.navbar.color};
   }
-  .navbar__theme__handler__circle{
+  .navbar__theme__handler__circle {
     background-color: ${(props) => props.theme.navbar.color};
     svg {
       fill: ${(props) => props.theme.navbar.background};
     }
   }
 `;
+const languages = [
+  {
+    label: "en",
+    value: "en",
+  },
+  {
+    label: "es",
+    value: "es",
+  },
+];
 export const Navbar = () => {
   const { theme, handleChangeTheme } = useContext(GlobalContext);
+  const router = useRouter();
+  const { locale } = router;
+  const handleChangeLanguage = (value) => {
+    router.push(router.pathname, router.pathname, { locale: value });
+  };
   return (
     <Container className={styles.navbar}>
       <div className={styles.navbar__content}>
         <div className={styles.navbar__left}>
-          <TextComponent text="{ Jordi Espinoza }" disableLocales />
+          <TextComponent
+            text="{ Jordi Espinoza }"
+            disableLocales
+            onClick={() => router.push("/")}
+          />
         </div>
         <div className={styles.navbar__right}>
+          <div className={styles.navbar__links}>
+            <Icon
+              nameIcon="AiFillGithub"
+              onClick={() => window.open(socialLinks.github, "_blank")}
+            />
+            <Icon
+              nameIcon="AiFillLinkedin"
+              onClick={() => window.open(socialLinks.linkedin, "_blank")}
+            />
+            <Icon
+              nameIcon="AiFillTwitterCircle"
+              onClick={() => window.open(socialLinks.twitter, "_blank")}
+            />
+          </div>
           <div className={styles.navbar__theme__handler}>
             <div
               className={`${styles.navbar__theme__handler__circle} navbar__theme__handler__circle`}
@@ -39,6 +78,17 @@ export const Navbar = () => {
             >
               <Icon nameIcon={`${theme === "light" ? "BsMoon" : "BsSun"}`} />
             </div>
+          </div>
+          {/* language handler */}
+          <div className={styles.navbar__language__handler}>
+            <select
+              value={locale}
+              onChange={(e) => handleChangeLanguage(e.target.value)}
+            >
+              {languages.map((language) => (
+                <option value={language.value}>{language.label}</option>
+              ))}
+            </select>
           </div>
         </div>
       </div>
--- components/skillset/index.js
+++ components/skillset/index.js
@@ -1,9 +1,17 @@
 import Icon from "components/icon";
 import React from "react";
 import styles from "./styles.module.sass";
+import styled from "styled-components";
+const Container = styled.div`
+  p,
+  svg,
+  a {
+    color: ${(props) => props.theme.textColorInverted};
+  }
+`;
 export const SkillSet = ({ data }) => {
   return (
-    <div className={styles.skillset}>
+    <Container className={styles.skillset}>
       {data.map((item, index) => (
         <div className={styles.skill__item} key={index}>
           <a href={item.link} target="_blank" rel="noopener noreferrer">
@@ -12,6 +20,6 @@ export const SkillSet = ({ data }) => {
           <p>{item.name}</p>
         </div>
       ))}
-    </div>
+    </Container>
   );
 };
