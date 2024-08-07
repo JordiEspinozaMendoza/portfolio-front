d407b8e682446bc27424ea3b457a351f0d20faa9
Jordi <71615543+JordiEspinozaMendoza@users.noreply.github.com>
2024-08-07 19:35:43 +0000

    fix: added show more text@

--- components/timeline/index.js
+++ components/timeline/index.js
@@ -2,6 +2,47 @@ import { useState } from "react";
 import Link from "next/link";
 import { handleGetTime } from "utils";
 
+const TimeLineItem = ({ experience }) => {
+  const [showMore, setShowMore] = useState(false);
+
+  return (
+    <li className="mb-10 ms-4" key={experience.company_url}>
+      <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+      <time className="mb-1 text-sm font-normal leading-none text-gray-400">
+        {handleGetTime(experience.time_data, experience.present)}
+      </time>
+      <h3 className="text-lg font-semibold text-white">{experience.title}</h3>
+      <Link
+        href={experience.company_url}
+        className="text-blue-500 underline hover:text-blue-700 w-fit text-sm"
+      >
+        {experience.company}
+      </Link>
+      {experience.description.length > 255 && !showMore ? (
+        <p className="mb-4 text-base font-normal text-gray-200 text-justify">
+          {experience.description.slice(0, 255)}...
+          <button
+            className="text-blue-500 underline hover:text-blue-700"
+            onClick={() => setShowMore(!showMore)}
+          >
+            Read more
+          </button>
+        </p>
+      ) : (
+        <p className="mb-4 text-base font-normal text-gray-200 text-justify">
+          {experience.description}
+          <button
+            className="text-blue-500 underline hover:text-blue-700"
+            onClick={() => setShowMore(!showMore)}
+          >
+            Show less
+          </button>
+        </p>
+      )}
+    </li>
+  );
+};
+
 export const Timeline = ({ experience }) => {
   const [showMore, setShowMore] = useState(false);
 
@@ -14,25 +55,8 @@ export const Timeline = ({ experience }) => {
           )
           .sort((a, b) => b.present - a.present)
           .slice(0, showMore ? experience.length : 4)
-          .map((experience, index) => (
-            <li className="mb-10 ms-4" key={index}>
-              <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-              <time className="mb-1 text-sm font-normal leading-none text-gray-400">
-                {handleGetTime(experience.time_data, experience.present)}
-              </time>
-              <h3 className="text-lg font-semibold text-white">
-                {experience.title}
-              </h3>
-              <Link
-                href={experience.company_url}
-                className="text-blue-500 underline hover:text-blue-700 w-fit text-sm"
-              >
-                {experience.company}
-              </Link>
-              <p className="mb-4 text-base font-normal text-gray-200 text-justify">
-                {experience.description}
-              </p>
-            </li>
+          .map((experience) => (
+            <TimeLineItem experience={experience} />
           ))}
       </ol>
       <div className="flex justify-center">
---
+++ test.md
@@ -0,0 +1,12 @@
+# Web rendering. Client Side Rendering and Server Side Rendering
+
+When we talk about web development and specifically about frontend development, we need to talk about web rendering patterns. A web rendering pattern it is how do we transform and display our source code to our clients. There are many ways to do this, but the 2 most used patterns in the past years are:
+
+- Server Side Rendering (SSR)
+- Client Side Rendering (CSR)
+
+Lets talk about this 2 approaches and how them work depending on your project, because the selected pattern for your proyect will depend on the requirements of your project.
+
+### Client Side Rendering (CRS)
+
+This pattern has become very popular with the 
